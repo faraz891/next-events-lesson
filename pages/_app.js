034b419b336f2293aaff373ffdd2a5b8e9282135
foxching <rechielagria@gmail.com>
2021-07-24 15:12:17 +0800

    add Head component

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
+import Head from 'next/head'
 import Layout from '../components/layout/layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="description" content="Nextjs Events" />
+        <meta name="keywords" content="Nextjs, Events" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
